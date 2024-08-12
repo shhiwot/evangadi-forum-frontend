caf20d31fba9cffe5baad17d32312630598ac6e9
shhiwot <shhiwot14@gmail.com>
2024-08-12 16:11:17 -0500

    update vite confg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Specify the output directory for the build
+    rollupOptions: {
+      input: {
+        main: "index.html", // Entry point for the application
+      },
+    },
+  },
+  base: "/", // Base public path when serving the app. Adjust if deploying to a subdirectory.
+});
